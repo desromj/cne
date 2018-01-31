90f06f0fb2ca9e8261c6a5ac26bc622d6758682e
Mike <desrochers.m.j@gmail.com>
2018-01-30 17:06:29 -0700

    Initial commit

---
+++ android/src/com/gbg/cne/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.gbg.cne;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.gbg.cne.CNEGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CNEGame(), config);
+	}
+}
---
+++ core/src/com/gbg/cne/CNEGame.java
@@ -0,0 +1,22 @@
+package com.gbg.cne;
+
+import com.badlogic.gdx.Game;
+
+
+public class CNEGame extends Game {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
